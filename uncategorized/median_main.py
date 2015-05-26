be6d06af2404e3827646604ecf138a46e905b4f1
Yigong <yigong@berkeley.edu>
2015-05-26 14:36:45 -0700

    delete dir ./uncategorized/

--- uncategorized/median_main.py
+++
@@ -1,6 +0,0 @@
-data_dir = '/Users/Yigong/Documents/beamCCD/data/'
-
-folder = data_dir + '04_28_14_Am_calibration/'
-img_idx = range(1,101)
-
-[med_img, img_array] = median_img(folder=folder, img_idx=img_idx, save_flag=False)
