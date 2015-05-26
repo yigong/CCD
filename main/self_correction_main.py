7d7cb7d18f9eecb1fc49cf3516c93a5d56d06ac6
Yigong <yigong@berkeley.edu>
2015-05-26 14:31:38 -0700

    delete dir ./main/

--- main/self_correction_main.py
+++
@@ -1,9 +0,0 @@
-from CCD.calibrate import self_subtraction
-
-folder = '/Users/Yigong/Research/photon_diagnostics/data/10_27_14_doubleThickness/measurement/'
-
-for image_index in np.arange(1,2):
-    file_name = folder + 'image_%s.fit' % image_index
-
-    a = self_subtraction(file_name, save_flag=True)
-
