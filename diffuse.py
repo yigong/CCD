d862d1ef00b63b82f3bf35d9be1df261e1717f0d
Yigong Zhang <ygzhang@n0005.scs00>
2015-06-22 15:06:39 -0700

    rewriting the Geant4 parser in procedual mode instead of object-oriented.

--- diffuse.py
+++ diffuse.py
@@ -1,33 +1,64 @@
 import numpy as np
-from IPython.core.debugger import Tracer
-debug_here = Tracer()
 
-def diffuseAndPixelize(xArray, yArray, zArray, dEArray, psfTable, pixelPlane):
+def XYZdE2track(xArray, yArray, zArray, dEArray, psfTable, pixelPlane):
     ''' compute the diffused energy deposition'''
-    nrow = psfTable[0].shape[0]
-    ncol = nrow
-    rows = np.arange(-(nrow/2), nrow/2., 1)
-    cols = rows
-    rowVectorPSF, colVectorPSF = np.meshgrid(rows, cols, indexing='ij')
-    # debug_here()
-    rowData = []
-    colData = []
-    EData = []
-    
-    for x, y, z, dE in zip(xArray, yArray, zArray, dEArray):
-        dEv = dE * computePSF(z, psfTable, pixelPlane)
-        rowVector = y + rowVectorPSF
-        colVector = x + colVectorPSF
-        
-        rowData.extend(rowVector.flatten())
-        colData.extend(colVector.flatten())
-        EData.extend(dEv.flatten())
-        # debug_here()
-    colEdges = np.arange(0, 36750.01, 10.5)
-    rowEdges = colEdges
-    image, a, b = np.histogram2d(rowData, colData, [colEdges, rowEdges], weights=EData)
-    # the first element is row; the second is col
-    return image
+
+    # find minimum and maximum locations in x and y
+    xMin = min(xArray)
+    xMax = max(xArray)
+    yMin = min(yArray)
+    yMax = max(yArray)
+    # find the row number and col number to use in the track
+    rowMin = np.ceil(yMin/10.5) - 3
+    rowMax = np.ceil(yMax/10.5) + 3
+    colMin = np.ceil(xMin/10.5) - 3
+    colMax = np.ceil(xMax/10.5) + 3
+    nrow = rowMax - rowMin
+    ncol = colMax - colMin
+    # create a track array: nrow x ncol
+    track = np.zeros((nrow, ncol))
+    # compute PSF grid
+    rowPSF = np.arange(-30, 30.01, 0.5)
+    colPSF = rowPSF
+    rowPSF2d, colPSF2d = np.meshgrid(rowPSF, colPSF, indexing='ij')
+    rowPSF1d = rowPSF2d.flatten()
+    colPSF1d = colPSF2d.flatten()
+    # store data
+    rows = []
+    cols = []
+    dE = [] 
+    for x_1step, y_1step, z_1step,dE_1step in zip(xArray, yArray, zArray, dEArray):
+        rows.extend(y_1step + rowPSF1d)
+        cols.extend(x_1step + colPSF1d)
+        dE.extend(dE_1step * computePSF(z_1step, psfTable, pixelPlane='top'))
+    rowEdges = np.arange(rowMin*10.5, rowMax*10.5 + 0.01, 10.5)
+    colEdges = np.arange(colMin*10.5, colMax*10.5 + 0.01, 10.5)
+    track, a, b = np.histogram2d(rows, cols, [rowEdges, colEdges], weights=dE) 
+    return track, rowMin, colMin
+##     nrow = psfTable[0].shape[0]
+##     ncol = nrow
+##     rows = np.arange(-(nrow/2), nrow/2., 1)
+##     cols = rows
+##     rowVectorPSF, colVectorPSF = np.meshgrid(rows, cols, indexing='ij')
+##     # debug_here()
+##     rowData = []
+##     colData = []
+##     EData = []
+##     
+##     for x, y, z, dE in zip(xArray, yArray, zArray, dEArray):
+##         dEv = dE * computePSF(z, psfTable, pixelPlane)
+##         rowVector = y + rowVectorPSF
+##         colVector = x + colVectorPSF
+##         
+##         rowData.extend(rowVector.flatten())
+##         colData.extend(colVector.flatten())
+##         EData.extend(dEv.flatten())
+##         # debug_here()
+##     colEdges = np.arange(0, 36750.01, 10.5)
+##     rowEdges = colEdges
+##     image, a, b = np.histogram2d(rowData, colData, [colEdges, rowEdges], weights=EData)
+##     # the first element is row; the second is col
+##     return image
     
     
 def computePSF(z, psfTable, pixelPlane):
@@ -35,11 +66,11 @@ def computePSF(z, psfTable, pixelPlane):
     if pixelPlane == 'bottom':
         zTable = np.arange(0, 650.5, 0.5)
         zAdjusted = min(zTable, key=lambda x:abs(x-z))        
-        return psfTable[int(2*zAdjusted)]
+        return psfTable[int(2*zAdjusted)].flatten()
     
     elif pixelPlane == 'top':
         zTable = np.arange(0, 650.5, 0.5)
         zAdjusted = 650 - min(zTable, key=lambda x:abs(x-z))    
-        return psfTable[int(2*zAdjusted)]
+        return psfTable[int(2*zAdjusted)].flatten()
     else:
         print 'pixelPlane is neither bottom nor top'
--- parse_10um.py
+++ parse_10um.py
@@ -1,14 +1,55 @@
+import os
+import glob
+import re
 import numpy as np
-from PyBeamDiag.G4Run import G4Run
-from PyBeamDiag.G4Event import G4Event
-import pickle
+from scipy.io import loadmat
+from PyBeamDiag.io import save_fits
+from PyBeamDiag.diffuse import XYZdE2track
 
-# cd to data dir
-fileDir = '/global/scratch/ygzhang/G4BeamDiag/step_size/out/10um'
-aRunEventList = []
-for fileIdx in range(15):
-  fileName = fileDir + '/G4W_%s_10um.out' %fileIdx
-  aThread = G4Run(fileName)
-  aThread.parse()
-  aRunEventList.extend(aThread.eventList) 
-pickle.dump(aRunEventList, open(fileDir+'/event_list.p', "wb"))
+# cd to G4out dir
+dataDir = '/global/scratch/ygzhang/G4BeamDiag/step_size/out_LRC/10um'
+os.chdir(dataDir)
+fileList = glob.glob('./G4W_*_10um.out')
+# load PSF
+psfDir = '/global/scratch/ygzhang/PyBeamDiag/data/psf.mat'
+psf_tmp = loadmat(psfDir)
+psfTable = np.array(psf_tmp['psf'])
+# initialization
+nTrk = 0 # count number of tracks
+for file in fileList[:2]:
+    print 'Now parsing file %s' % file
+    endOfFile = False
+    f = open(file, 'r')
+    while not endOfFile:
+        x_tmp = []
+        y_tmp = []
+        z_tmp = []
+        dE_tmp = []
+        for line in f:
+            if line[0] == '*' and x_tmp:
+                break
+            elif line == '':
+                endOfFile = True             
+                print 'Reaches EOF'
+            elif 'physiTracker' not in line:
+                continue
+            elif 'physiTracker' in line:
+                lineSplit = re.split(r'\s*[(), \s]\s*', line)
+                if lineSplit[0] == '':
+                    lineSplit = lineSplit[1:]
+                x_tmp.append(float(lineSplit[0]))
+                y_tmp.append(float(lineSplit[1]))
+                z_tmp.append(float(lineSplit[2]))
+                dE_tmp.append(float(lineSplit[6]))
+        x = np.array(x_tmp) + 37000./2
+        y = np.array(y_tmp) - 2000.
+        z = np.array(z_tmp)
+        dE = np.array(dE_tmp)
+        track, rowMin, colMin = XYZdE2track(x, y, z, dE, psfTable, \
+                pixelPlane='top')
+        del x_tmp, y_tmp, z_tmp, dE_tmp, x, y, z, dE
+        nTrk += 1
+        print 'Now parsing track #%d' % (nTrk)
+        print '(rowMin, colMin) = %d, %d' % (rowMin, colMin)
+        save_fits(track, './%d.fits' % nTrk, False)
+        
