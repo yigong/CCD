b056459efec100a2aadb183c882546394f29d016
Yigong Zhang <ygzhang@n0000.scs00>
2015-06-23 18:00:05 -0700

    1) getting G4parser done: parse_10um.py 2) after parsing, saving each track separately in #.fits

--- parse_10um.py
+++ parse_10um.py
@@ -1,10 +1,11 @@
 import os
 import glob
 import re
+import inspect
 import numpy as np
 from scipy.io import loadmat
-from PyBeamDiag.io import save_fits
 from PyBeamDiag.diffuse import XYZdE2track
+from astropy.io import fits
 
 # cd to G4out dir
 dataDir = '/global/scratch/ygzhang/G4BeamDiag/step_size/out_LRC/10um'
@@ -15,41 +16,50 @@ psfDir = '/global/scratch/ygzhang/PyBeamDiag/data/psf.mat'
 psf_tmp = loadmat(psfDir)
 psfTable = np.array(psf_tmp['psf'])
 # initialization
+lineNum = 0
 nTrk = 0 # count number of tracks
 for file in fileList[:2]:
     print 'Now parsing file %s' % file
     endOfFile = False
     f = open(file, 'r')
     while not endOfFile:
-        x_tmp = []
-        y_tmp = []
-        z_tmp = []
-        dE_tmp = []
-        for line in f:
-            if line[0] == '*' and x_tmp:
-                break
-            elif line == '':
-                endOfFile = True             
-                print 'Reaches EOF'
-            elif 'physiTracker' not in line:
-                continue
-            elif 'physiTracker' in line:
-                lineSplit = re.split(r'\s*[(), \s]\s*', line)
-                if lineSplit[0] == '':
-                    lineSplit = lineSplit[1:]
-                x_tmp.append(float(lineSplit[0]))
-                y_tmp.append(float(lineSplit[1]))
-                z_tmp.append(float(lineSplit[2]))
-                dE_tmp.append(float(lineSplit[6]))
-        x = np.array(x_tmp) + 37000./2
-        y = np.array(y_tmp) - 2000.
-        z = np.array(z_tmp)
-        dE = np.array(dE_tmp)
-        track, rowMin, colMin = XYZdE2track(x, y, z, dE, psfTable, \
-                pixelPlane='top')
-        del x_tmp, y_tmp, z_tmp, dE_tmp, x, y, z, dE
-        nTrk += 1
-        print 'Now parsing track #%d' % (nTrk)
-        print '(rowMin, colMin) = %d, %d' % (rowMin, colMin)
-        save_fits(track, './%d.fits' % nTrk, False)
-        
+        try: 
+            x_tmp = []
+            y_tmp = []
+            z_tmp = []
+            dE_tmp = []
+            for line in f:
+                lineNum += 1
+                if line[0] == '*' and x_tmp:
+                    break
+                elif line == '':
+                    endOfFile = True             
+                    print 'Reaches EOF'
+                elif 'physiTracker' not in line:
+                    continue
+                elif 'physiTracker' in line:
+                    lineSplit = re.split(r'\s*[(), \s]\s*', line)
+                    if lineSplit[0] == '':
+                        lineSplit = lineSplit[1:]
+                    x_tmp.append(float(lineSplit[0]))
+                    y_tmp.append(float(lineSplit[1]))
+                    z_tmp.append(float(lineSplit[2]))
+                    dE_tmp.append(float(lineSplit[6]))
+            print 'Now we are at line #%s' % lineNum
+            x = np.array(x_tmp) + 37000./2
+            y = np.array(y_tmp) - 2000.
+            z = np.array(z_tmp)
+            dE = np.array(dE_tmp)
+            track, rowMin, colMin = XYZdE2track(x, y, z, dE, psfTable, \
+                    pixelPlane='top')
+            del x_tmp, y_tmp, z_tmp, dE_tmp, x, y, z, dE
+            nTrk += 1
+            print 'Now parsing track #%d' % (nTrk)
+            print '(rowMin, colMin) = %d, %d' % (rowMin, colMin)
+            h = fits.Header()
+            h['rowMin'] = rowMin
+            h['colMin'] = colMin
+            fits.writeto('./%d.fits' % nTrk, track, h, clobber=True)
+        except ValueError:
+            print 'ValueError'
+            continue
