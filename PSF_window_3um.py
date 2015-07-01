9b37d641f4b890333cd0f91b5375b82dfdf6c1d6
Yigong Zhang <ygzhang@n0004.scs00>
2015-07-01 15:18:55 -0700

    recon 1um data

--- PSF_window_3um.py
+++ PSF_window_3um.py
@@ -10,7 +10,7 @@ chdir(dataDir)
 Files = glob('./*.mat')
 numTracks = len(Files)
 
-recon = np.zeros(4)
+var = np.zeros(4)
 x_CCD = np.zeros(numTracks)
 y_CCD = np.zeros_like(x_CCD)
 alpha = np.zeros_like(x_CCD)
@@ -20,21 +20,21 @@ print 'after initialization'
 
 for i, matFile in enumerate(Files):
     file = openFile(matFile)
-    recon = np.squeeze(file.root.recon)
+    var = np.squeeze(file.root.var)
 #    recon[0] = file.root.recon.alpha[0][0]
 #    recon[1] = file.root.recon.beta[0][0]
 #    recon[2] = file.root.recon.startPix[0][0]
 #    recon[3] = file.root.recon.startPix[1][0]
     # ele0:row ele1:col ele2:alpha ele3:beta
 
-    x_CCD[i] = 10.5/2 + recon[2] * 10.5 - 37000./2
-    y_CCD[i] = 10.5/2 + recon[3] * 10.5 + 2000.
-    alpha[i] = recon[0]
-    beta[i]  = recon[1]
+    x_CCD[i] = 10.5/2 + var[2] * 10.5 - 37000./2
+    y_CCD[i] = 10.5/2 + var[3] * 10.5 + 2000.
+    alpha[i] = var[0]
+    beta[i]  = var[1]
 
 print 'finish loading data'
 x_window = x_CCD - y_CCD * tan(deg2rad(alpha)) 
 x_window[(90<alpha) & (alpha<270)] = float('inf')
-dump(x_window, open('../x_window.p', 'wb'))
-dump([x_window, x_CCD, y_CCD, alpha, beta], open('../ridgeFollow.p', 'wb'))
+# dump(x_window, open('../x_window.p', 'wb'))
+dump([x_window, x_CCD, y_CCD, alpha, beta], open('../track_recon.p', 'wb'))
 print 'data is dumped'
--- matlab/TRACK_recon_1um.m
+++ matlab/TRACK_recon_1um.m
@@ -9,21 +9,21 @@ parfor i = 1:fileNum
     ['recon # ', num2str(i)]
     if ~isfield(recon_tmp, 'err')
         ['save # ', num2str(i)]
-        recon = zeros(1,4);
-        recon(1) = recon_tmp.alpha;
-        recon(2) = recon_tmp.beta;
+        var = zeros(1,4);
+        var(1) = recon_tmp.alpha;
+        var(2) = recon_tmp.beta;
         % convert relative start position to absolute position
         h = fitsinfo(fileList(i).name);
         minRow_track = h.PrimaryData.Keywords{14}
         minCol_track = h.PrimaryData.Keywords{15}
         startPix = recon_tmp.EdgeSegments.startCoordinatesPix;
-        recon(3) = minRow_track + startPix(1)
-        recon(4) = minCol_track + startPix(2)
+        var(3) = minRow_track + startPix(1)
+        var(4) = minCol_track + startPix(2)
        % % save to file, then delete
         saveName = sprintf('../mat/%d.mat', i);
-        saveParFor(saveName, recon, '-v7.3');
+        saveParFor(saveName, var, '-v7.3');
         % free up memory
-        recon = [];
+        var= [];
         recon_tmp = [];
         track = [];
         h = [];
