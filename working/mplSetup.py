bc7b019332cdd3284435bf728a4dc722b70b6e44
Yigong <yigong@berkeley.edu>
2015-07-09 13:23:01 -0700

    compare PSF for different step sizes

--- working/mplSetup.py
+++
@@ -1,8 +0,0 @@
-import matplotlib
-fontSize = 16
-matplotlib.rcParams["font.size"] = fontSize
-matplotlib.rcParams["xtick.labelsize"] = fontSize
-#matplotlib.rcParams["xtick.major.size"] = 16
-matplotlib.rcParams["ytick.labelsize"] = fontSize
-#matplotlib.rcParams["ytick.major.size"] = 16
-matplotlib.rcParams["legend.fontsize"] = fontSize
